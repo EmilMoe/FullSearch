c2d6c5512418fa90e0db75c001a3963691a8791b
Emil Moe <emil@moegroup.dk>
2019-05-15 14:36:01 +0200

    Publish config

--- src/FullSearchServiceProvider.php
+++ src/FullSearchServiceProvider.php
@@ -25,5 +25,6 @@ class FullSearchServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__ .'/routes/web.php');
         $this->mergeConfigFrom(__DIR__ .'/config.php', 'full-search');
+        $this->publishes([__DIR__. '/config.php' => config_path('full-search.php'),]);
     }
 }
